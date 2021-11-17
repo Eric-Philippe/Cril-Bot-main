0710eeae85e51180260a03d55b1b668a4d7ae258
Eric PHILIPPE <ericphlpp@gmail.com>
2021-11-17 09:42:09 +0100

    test

---
+++ Cril-Bot-main/index.js
@@ -0,0 +1,20 @@
+const { client } = require("./utils/client");
+
+const { TOKEN } = require("./token.json");
+
+const { PREFIX } = require("./config.json");
+
+client.on("ready", () => {
+  console.log(`Logged into: ${client.user.tag}`);
+});
+
+//Test
+client.on("messageCreate", (msg) => {
+  if (msg.content === PREFIX + "chat") {
+    msg.reply(
+      "https://tenor.com/view/sleep-cat-two-cat-cat-cat-love-gif-23651667"
+    );
+  }
+});
+
+client.login(TOKEN);
