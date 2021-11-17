2960e303ccb400f90b8b00bcc212ba8d475ba343
Eric PHILIPPE <ericphlpp@gmail.com>
2021-11-17 11:22:25 +0100

    Edit

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const { client } = require("./utils/client");
 
 const { TOKEN } = require("./token.json");
 
-const { PREFIX } = require("./config.json");
-
 const { cmdGrabber } = require("./commands/commandsGrabber");
 
 client.on("ready", () => {
   console.log(`Logged into: ${client.user.tag}`);
 });
 
-//Test Comment
+//Msg Event
 client.on("messageCreate", (msg) => {
   if (msg.author.bot) return;
   if (msg.author.id === client.user.id) return;
