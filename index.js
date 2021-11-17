ce33a32faf0b69933fa1e37b0c45062f9451327b
Eric PHILIPPE <ericphlpp@gmail.com>
2021-11-17 09:52:12 +0100

    Test Push

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const { client } = require('./utils/client');
+const { client } = require("./utils/client");
 
-const { TOKEN } = require('./token.json');
+const { TOKEN } = require("./token.json");
 
-const { PREFIX } = require('./config.json');
+const { PREFIX } = require("./config.json");
 
-client.on('ready', () => {
-    console.log(`Logged into: ${client.user.tag}`);
-})
+client.on("ready", () => {
+  console.log(`Logged into: ${client.user.tag}`);
+});
 
-client.on('messageCreate', msg => {
-    if (msg.content === PREFIX + "chat") {
-        msg.reply("https://tenor.com/view/sleep-cat-two-cat-cat-cat-love-gif-23651667")
-    }
+//Test Comment
+client.on("messageCreate", (msg) => {
+  if (msg.content === PREFIX + "chat") {
+    msg.reply(
+      "https://tenor.com/view/sleep-cat-two-cat-cat-cat-love-gif-23651667"
+    );
+  }
 });
 
 client.login(TOKEN);
