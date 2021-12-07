8d5c3c396e725e19133d6349bcd1c312635a3f86
Eric PHILIPPE <ericphlpp@gmail.com>
2021-12-07 11:25:16 +0100

    Commands Clean Up

--- helpSystem/chatBot.js
+++ helpSystem/chatBot.js
@@ -1,14 +1,20 @@
 const Discord = require("discord.js");
 
+// Import target function
 const AnswerZero = require("./answerLevelZero");
 const Find_Activty = require("./find_activity/find_activity");
 const Unsubscribe = require("./unsubscribe/unsubscribe");
 
+// Helper import
 const { questionPicker } = require("./questionPicker");
 const { filter } = require("./filter");
 
+/**
+ * chatBot Help System
+ */
 module.exports = class chatBot {
   /**
+   *  Constructor of the chatBot System
    *
    * @param {Discord.Message} msg
    */
@@ -49,7 +55,7 @@ module.exports = class chatBot {
    * @param {Discord.Message} msg
    */
   async __init__(msg) {
-    this.answer_array = await questionPicker(msg.content);
+    this.answer_array = await questionPicker(msg.content); // Pick an array of three element to find the closest answer
     if (this.answer_array[0][1] > 20) return; // Deny if the answers found are too far from the question
 
     let user_filter = await filter(msg, this.answer_array[0]);
@@ -59,8 +65,10 @@ module.exports = class chatBot {
   }
 
   async __selector__(step) {
-    let level = 0;
-    console.log(this.answer_array);
+    let level = 0; // Default level of the question
+    // Level 0 => Basic Answer
+    // Level 1 => Basic Answer after confirmation
+    // Level 2 => Full system for the answer
     switch (this.answer_array[step][2]) {
       case "FIND_MOODLE":
         this.msg_embed = await AnswerZero.find_moodle(this.msg, true);
@@ -100,62 +108,76 @@ module.exports = class chatBot {
         break;
     }
 
-    if (!this.msg_embed) return;
-    this.buttonCollector(this.msg, this.msg_embed, level);
+    if (!this.msg_embed) return; // Error Handler (time)
+    this.buttonCollector(this.msg, this.msg_embed, level); // Collect if the user is satsified or not
   }
 
   /**
+   *  Collect the satisfaction of the user about the answer given
    *
    * @param {Discord.Message} msg_user
    * @param {Discord.Message} msg_bot
    */
   buttonCollector(msg_user, msg_bot, level) {
+    // [ButtonOnly, AuthorOnly]
     const filter = (interaction) =>
       interaction.user.id === msg_user.author.id && interaction.isButton();
     const collector = msg_bot.createMessageComponentCollector({
       filter,
       max: 1,
-      time: 1000 * 20,
+      time: 1000 * 60 * 2, // 2 minutes
       errors: ["time"],
     });
 
     collector.on("collect", async (i) => {
       switch (i.customId) {
-        case "Happy":
+        case "Happy": // Satisfied
           this.__launcher__(level);
           break;
-        case "Unhappy":
+        case "Unhappy": // Unsatisfied
           if (this.step === 2) {
+            // Case of all the answer unsatisfied
             msg_user.reply(
               "Je n'ai plus de réponse à vous apporter, merci de contacter un @Responsable !"
             );
           } else {
+            // Change the current step to give an other solution
             this.step = this.step + 1;
             msg_user.reply("Je m'occupe de vous. . .").then((m) => {
-              i.de;
+              // Clean message
               setTimeout(() => {
                 m.delete();
               }, 1000 * 3);
             });
             this.msg_embed.delete();
-            this.__selector__(this.step);
+            this.__selector__(this.step); // Launch a new selection
           }
           break;
       }
 
       await i.deferUpdate();
     });
-
-    collector.on("end", (collected, reason) => {});
   }
 
+  /**
+   * Default function for basic answer
+   *
+   * @returns {Discord.Message}
+   */
   default_func_validation() {
     return this.msg.reply("Merci d'avoir répondu !");
   }
 
+  /**
+   *  Launch the targeted function
+   *
+   * @param {Number} level
+   * @returns {Function} Func
+   */
   __launcher__(level) {
     try {
-      if (level === 2) return new this.func(this.msg, this.msg_embed);
+      if (level === 2) return new this.func(this.msg, this.msg_embed); // Level 2 => Class Launcher
+      // Level 1 - 0 => function launcher
       this.func(this.msg);
     } catch (err) {
       console.log(err);
--- helpSystem/filter.js
+++ helpSystem/filter.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
-const { ROLES, CHANNELS } = require("../ressources.json");
-const CoolDown_Min = 2;
+const { ROLES, CHANNELS } = require("../ressources.json"); // Stuff needed for the filter
+const CoolDown_Hour = 1; // Time in hour between two help
 
-const talkedRecently = new Set();
+const talkedRecently = new Set(); // CoolDown SET
 
 /**
  * Filter the user for the chatBot
@@ -14,6 +14,7 @@ const talkedRecently = new Set();
  * @returns {Boolean} boolean
  */
 const filter = async function (msg, answer) {
+  // Check if good channel (Avoid Flood)
   if (
     msg.channel.id != CHANNELS.SUPPORT_CHANNEL &&
     !CHANNELS.ACTIVITIES_CHANNEL.includes(msg.channel.id)
@@ -22,7 +23,10 @@ const filter = async function (msg, answer) {
   let member = msg.member;
   let member_roles = member.roles.cache.toJSON();
   let isMod = member_roles.some((item) => ROLES.MOD_ROLES.includes(item.id));
+  // Check if not Admin (No need)
   if (isMod) return;
+
+  // Only launch FIND_ACTIVITY HELP in not support channel if the answers are close
   if (
     msg.channel.id != CHANNELS.SUPPORT_CHANNEL &&
     answer[2] != "FIND_ACTIVITY"
@@ -30,12 +34,14 @@ const filter = async function (msg, answer) {
     if (answer[1] > 6) return;
   }
   let user = msg.author;
+  // CoolDown Filter
   if (talkedRecently.has(user.id)) return;
+  // Add CoolDown then delete it after 1h
   talkedRecently.add(user.id);
   setTimeout(() => {
     talkedRecently.delete(user.id);
-  }, 1000 * 60 * CoolDown_Min);
+  }, 1000 * 60 * 60 * CoolDown_Hour);
   return true;
 };
 
-exports.filter = filter;
+exports.filter = filter; // Export filter chatBot
