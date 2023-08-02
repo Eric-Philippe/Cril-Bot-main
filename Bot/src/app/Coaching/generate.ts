f6f1566cf17cef7c112d47d5371cde97f4aa7487
Eric-Philippe <ericphlpp@gmail.com>
2023-08-02 22:16:56 +0200

    Clean

--- Bot/src/app/Coaching/generate.ts
+++
@@ -1,6 +0,0 @@
-import { CommandInteraction } from "discord.js";
-import EmbedsCoaching from "./Embeds";
-
-export default function generate(i: CommandInteraction) {
-  i.channel.send(EmbedsCoaching.welcomeEmbed());
-}
