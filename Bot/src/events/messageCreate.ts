36fc9b27c9b4c9aa4fb12aeaaae05f3eec34ea42
Eric-Philippe <ericphlpp@gmail.com>
2024-04-23 08:24:47 +0200

    Add Anti-Raid System - (Avoid any weird discord invite links from hacked accounts)

--- Bot/src/events/messageCreate.ts
+++ Bot/src/events/messageCreate.ts
@@ -3,10 +3,45 @@ import { Client, Events, PermissionFlagsBits } from "discord.js";
 import Entry from "../app/Entry/Entry";
 import { CHAN_DPLACE, ENTRY_CHANNELS } from "../config/config.guild";
 import threadLogger from "../utils/ThreadLogger";
+import Logger from "../logger/Logger";
+import { LogsGeneralLevels } from "../logger/LogsGeneral.Levels";
+
+const problematicUsers = new Set();
 
 export default (client: Client) => {
   client.on(Events.MessageCreate, async (msg) => {
     if (msg.author.bot) return;
+
+    // If the message contains a discord invite link
+    if (
+      msg.content.match(/discord.gg/) &&
+      !msg.member.permissions.has(PermissionFlagsBits.DeafenMembers)
+    ) {
+      await msg.delete();
+
+      if (problematicUsers.has(msg.author.id)) {
+        try {
+          await msg.member.kick();
+          await msg.author.send(
+            "Please do not send discord invite links in the server"
+          );
+
+          Logger.logGeneral(
+            LogsGeneralLevels.WARNING,
+            `User ${msg.author.tag} sent more than one discord invite link in the server and gets kicked`
+          );
+
+          return;
+        } catch (e) {
+          Logger.logError(e);
+        }
+      } else {
+        problematicUsers.add(msg.author.id);
+        setTimeout(() => {
+          problematicUsers.delete(msg.author.id);
+        }, 1000 * 60 * 5);
+      }
+    }
 
     const channel = msg.channel;
     if (
