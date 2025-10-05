580dd2996d09f4b7139b9f4e830d2661a0eaeede
Eric-Philippe <ericphlpp@gmail.com>
2025-10-05 16:28:07 +0200

    Fix database URL in config to use correct hostname

--- Bot/src/config/config.database.ts
+++ Bot/src/config/config.database.ts
@@ -6,6 +6,6 @@ if (!POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_DB) {
   throw new Error("Missing environment variables");
 }
 
-const DB_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@bot-db-cril:5432/${POSTGRES_DB}`;
+const DB_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@cril-db:5432/${POSTGRES_DB}`;
 
 export { DB_URL, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB };
